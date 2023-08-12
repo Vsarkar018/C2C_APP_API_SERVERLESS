def0ded147a25e0a88f1bef86ea6c44a2194e3df
Vishal Sarkar <vishalsarkar018@gmail.com>
2023-08-12 23:11:49 +0530

    ImageAPI added

--- product_service/lib/product_service-stack.ts
+++ product_service/lib/product_service-stack.ts
@@ -2,17 +2,17 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { ServiceStack } from "./service_stack";
 import { ApiGatewayStack } from "./api_gateway_stack";
-import { S3BucketStack } from "./S3Bucket-stack";
+import { S3BucketStack } from "./s3Bucket_stack";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const { bucket } = new S3BucketStack(this, "productImages");
+    const BucketStack = new S3BucketStack(this, "productImages");
     const { productService, categoryService, dealService, imageService } =
       new ServiceStack(this, "ProductService", {
-        bucket: bucket.bucketName,
+        bucket: BucketStack.bucket.bucketName,
       });
-    bucket.grantReadWrite(imageService);
+    BucketStack.bucket.grantReadWrite(imageService);
     new ApiGatewayStack(this, "ProductApiGateway", {
       productService,
       categoryService,
--- product_service/src/ImageAPI.ts
+++ product_service/src/ImageAPI.ts
@@ -1,7 +1,14 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
-import { S3 } from "aws-sdk";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { v4 as uuid } from "uuid";
-const S3Client = new S3();
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: "AKIAYCBDFT3CQ2EP6KUV",
+    secretAccessKey: "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",
+  },
+});
 
 export const handler = async (
   event: APIGatewayEvent,
@@ -9,12 +16,14 @@ export const handler = async (
 ): Promise<APIGatewayProxyResult> => {
   const file = event.queryStringParameters?.file;
   const fileName = `${uuid()}__${file}`;
-  const s3Params = {
+  const s3Params = new PutObjectCommand({
     Bucket: process.env.BUCKET_NAME,
     Key: fileName,
     ContentType: "image/jpeg",
-  };
-  const url = await S3Client.getSignedUrlPromise("putObject", s3Params);
+  });
+  const url = await getSignedUrl(s3Client, s3Params, {
+    expiresIn: 1000 * 60 * 10,
+  });
   console.log("UPLOAD URL :", s3Params, url);
 
   return {
